a6086aa0f2ded666d4fcd95df035210c2261b16c
gray <>
2023-03-26 20:40:23 +0800

    optimization code

--- constant/index.js
+++ constant/index.js
@@ -11,7 +11,9 @@ const state = {
     failByMulti: 4,
     failByUnknown: 5
 }
+const chainDesc = ['arbitrum', 'ethereum', 'optimism', 'starknet', 'zksyncera', 'zksynclite','polygon','metis','boba','bsc']
 
 
 module.exports.confirmStatus = confirmStatus;
 module.exports.state = state;
+module.exports.chainDesc = chainDesc;
