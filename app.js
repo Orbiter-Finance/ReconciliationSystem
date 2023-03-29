2cb373c6045bf9c133c47ace74b95b9024e62d41
gray <>
2023-03-29 14:04:34 +0800

    fixed cross

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const logger = require('./utils/logger');
 
 app.use(async (ctx, next)=> {
     ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , token');
     ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     if (ctx.method == 'OPTIONS') {
       ctx.body = 200; 
